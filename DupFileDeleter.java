9df2402b7f43d0eae5bf57faecd6f8894ac40352
Jacob Kim <jkim57@emich.edu>
2023-09-11 11:31:53 -0400

    Update things to do

--- DupFileDeleter.java
+++ DupFileDeleter.java
@@ -1,7 +1,24 @@
 public class DupFileDeleter
 {
+	/*Things To Do:
+	 * Access Files
+	 * Display File Names
+	 * Menu Format
+	 * Delete Files that are duplicated
+	 * Give an option to delete only one of the files that is duplicated
+	 * 
+	 * 
+	 */
 	public static void main(String[] args) {
+
 		System.out.println("Hello World!");
+
+	
+
 	}
+
+
+
+
+
 }
-// c:\Users\jacob\Downloads
