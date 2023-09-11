662732c856b23db97187c83210866231eb446cdf
Jacob Kim <jkim57@emich.edu>
2023-09-11 14:14:53 -0400

    update

--- DupFileDeleter.java
+++ DupFileDeleter.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class DupFileDeleter
 {
 	/*Things To Do:
@@ -6,18 +12,67 @@ public class DupFileDeleter
 	 * Menu Format
 	 * Delete Files that are duplicated
 	 * Give an option to delete only one of the files that is duplicated
-	 * 
-	 * 
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		//Get Files
+		
+		File directory = new File("C:\\Users\\jacob\\Desktop\\FileDupDeleter\\Files");
+		List<String> fileList = new ArrayList<>();
 
-	
+		for (File file: directory.listFiles()) {
+			fileList.add(file.getName());
+		}
+		String first = "";
+		for (int i=0; i<fileList.size(); i++) {
+			first = fileList.get(0);
+		}
 
+		//Display FileNames
+		// for(int i = 0; i<fileList.size(); i++) {
+		// 	String fileName = fileList.get(i);
+		// 	System.out.println(fileName);
+		// }
+
+
+		//Create Menu
+		String Menu = "Duplicate File Deleter Menu \n" 
+						+ "1: Display File Names \n"
+						+ "2: Delete Duplicated Files \n"
+						+ "3: Delete files of a specific file type \n"
+						+ "4: Delete only one of duplciated files (webm) \n"
+						+ "5: Exit";
+		System.out.println(Menu);
+		Scanner Input = new Scanner(System.in);
+		int UserInput = Input.nextInt();
+		while (UserInput != 5) {
+		switch (UserInput) {
+			
+			case 1:		;
+					break;
+			case 2: System.out.println("you chose 2");
+					break;
+			case 3: System.out.println("you chose 3");
+					break;
+			case 4: System.out.println("you chose 4");
+					break;
+			
+		}
+		UserInput = Input.nextInt();
+		if (UserInput == 5) {
+			System.out.println("Exiting");
+		}
+	}	
+		Input.close();
 	}
 
+	public static void DisplayList(String list[]) {
 
+		for(int i = 0; i<list.size(); i++) {
+			String fileName = list.get(i);
+			System.out.println(fileName);
+		}
+	}
 
 
 
